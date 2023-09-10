f96760c5f1c4c54038486984c8bfff9500bc1d35
caseyrunn <caseyrunn@gmail.com>
2023-09-10 13:48:04 -0600

    Add pseudocode for playRound

--- script.js
+++ script.js
@@ -9,5 +9,21 @@ function getComputerChoice(){
     }
 }
 
+function playRound(playerSelection, computerSelection){
+    //if player and computer have same selection
+        //it's a tie
+    //if player selects rock and computer selects paper
+        //computer wins
+    //if player selects rock and computer selects scissors
+        //player wins
+    //if player selects paper and computer selects rock
+        //player wins
+    //if player selects paper and computer selects scissors
+        //computer wins
+    //if player selects scissors and computer selects rock
+        //computer wins
+    //if player selects scissors and computer selects paper
+        //player wins
+}
 
 console.log(getComputerChoice());
