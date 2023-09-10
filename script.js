e837b79a190eaf2fe813ff9c1ffb06c7b6bd8efe
caseyrunn <caseyrunn@gmail.com>
2023-09-10 13:14:40 -0600

    Pseudocode for getComputerChoice function

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice(){
+//  Randomly generate number 1, 2, or 3;
+//  If 1, return rock 
+//  If 2, return paper 
+//  If 3, return scissors;
+}
+
+function oneTwoOrThree(){
+    //  Randomly generate number 0-1;
+    //  Multiply number by 3;
+    //  Round down to closest whold number;  
+    }
