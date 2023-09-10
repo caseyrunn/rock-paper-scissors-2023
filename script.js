87234cc3d3d897ef03e2ee2ab95dae2b5b9b6a05
caseyrunn <caseyrunn@gmail.com>
2023-09-10 13:37:42 -0600

    Integrate zeroOneorTwo into getComputerChoice

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function getComputerChoice(){
-//  Randomly generate number 1, 2, or 3;
-//  If 1, return rock 
-//  If 2, return paper 
-//  If 3, return scissors;
+    let number = Math.floor(Math.random()*3);
+    if (number === 0){
+        console.log(0);
+    } else if (number ===1){
+        console.log(1);
+    } else {
+        console.log(2);
+    }
 }
 
-function zeroOneorTwo(){
-    return Math.floor(Math.random()*3);  
-    }
+
+getComputerChoice();
